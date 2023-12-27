5fa59b4051fb0fae197768335dae1b06d3b6f22d
Pablo Ferrari <pablo.ferrari@me.com>
2023-12-27 19:23:01 -0300

    Update index.js

--- backend/environments/index.js
+++ backend/environments/index.js
@@ -6,6 +6,7 @@ const DB_PASSWORD = (process.env.DB_PASSWORD || '')
 const DB_NAME = (process.env.DB_NAME || '')
 const DB_PORT = (process.env.DB_PORT || '')
 const DB_HOST = (process.env.DB_HOST || '')
+const API_KEY = (process.env.API_KEY || '')
 
 module.exports = {
     PORT,
@@ -14,4 +15,5 @@ module.exports = {
     DB_NAME,
     DB_PORT,
     DB_HOST,
+    API_KEY
 }
