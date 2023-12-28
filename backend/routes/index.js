00ee213e94b1144289f0ca360c325c6490b9e606
Pablo Ferrari <pablo.ferrari@me.com>
2023-12-28 18:26:44 -0300

    refactoring routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const { teamIdRequest } = require('../middlewares');
 const TeamsController = require('../controllers/TeamController');
-const router = express.Router();
 
-router.route('/teams/:teamId/matches').get(teamIdRequest, TeamsController.getMatches);
-router.route('/teams/:teamId/players').get(teamIdRequest, TeamsController.getPlayers);
+function initializeRoutes(app) {
+    const router = express.Router();
 
-module.exports = router;
+    router.route('/teams/').get(TeamsController.getTeams);
+    router.route('/teams/:teamId/matches').get(teamIdRequest, TeamsController.getMatchesByTeam);
+    router.route('/teams/:teamId/players').get(teamIdRequest, TeamsController.getPlayersByTeam);
+
+    router.route('/*').all((req, res) => res.status(404).json({ message: "Route not found" }));
+
+    app.use(router);
+}
+
+module.exports = { initializeRoutes };
