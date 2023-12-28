333cdf0c788e7f3892c932ce23337599389f63f5
Pablo Ferrari <pablo.ferrari@me.com>
2023-12-28 18:25:45 -0300

    Update TeamController.js

--- backend/controllers/TeamController.js
+++ backend/controllers/TeamController.js
@@ -1,18 +1,31 @@
-
-// ----------------- IMPORT PACKAGES ----------------- //
-const db = require('../config/database');
-
+const TeamService = require('../services/team.service');
+const MatchService = require('../services/match.service');
+const PlayerService = require('../services/player.service');
 
 const TeamController = {}
 
-TeamController.get
+const handleRequest = async (res, serviceFunction, params) => {
+    try {
+        const result = await serviceFunction(params);
+        res.json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
+};
 
-TeamController.getMatches = async (req, res) => {
-    res.json([]);
-}
-TeamController.getPlayers = async (req, res) => {
-    res.json([]);
-}
+TeamController.getTeams = async (req, res) => {
+    await handleRequest(res, TeamService.getTeams);
+};
 
+TeamController.getMatchesByTeam = async (req, res) => {
+    const { teamId } = req.params;
+    await handleRequest(res, MatchService.getByTeam, teamId);
+};
+
+TeamController.getPlayersByTeam = async (req, res) => {
+    const { teamId } = req.params;
+    await handleRequest(res, PlayerService.getByTeam, teamId);
+};
 
 module.exports = TeamController;
