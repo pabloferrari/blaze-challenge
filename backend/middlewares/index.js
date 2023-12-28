a76bd3c4379b25e4d23dcd356e5a90b4bee0078c
Pablo Ferrari <pablo.ferrari@me.com>
2023-12-28 18:26:09 -0300

    request validator

--- backend/middlewares/index.js
+++ backend/middlewares/index.js
@@ -1,11 +1,26 @@
+const TeamService = require('../services/team.service');
 
-const teamIdRequest = (req, res, next) => {
+const teamIdRequest = async (req, res, next) => {
+    try {
+        const { teamId } = req.params;
 
-    const { teamId } = req.params;
-    console.log(`teamIdRequest(${teamId})`)
-    return next()
-}
+        if (!Number.isNaN(Number(teamId))) {
+            const team = await TeamService.getTeamById(teamId);
+
+            if (team) {
+                return next();
+            } else {
+                return next({ status: 404, message: 'Team not found' });
+            }
+        } else {
+            return next({ status: 400, message: 'Invalid teamId' });
+        }
+    } catch (error) {
+        console.error('Error in teamIdRequest middleware:', error);
+        return next({ status: 500, message: 'Internal Server Error' });
+    }
+};
 
 module.exports = {
-    teamIdRequest,
-}
+    teamIdRequest
+};
